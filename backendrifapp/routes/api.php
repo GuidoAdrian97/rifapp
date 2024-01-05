f13bb2355034850e162685c25e075ed597bce90f
GuidoAdrian97 <guidoadrian97@hotmail.com>
2024-01-05 15:45:48 -0500

    login v1

--- backendrifapp/routes/api.php
+++ backendrifapp/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Socialite\Facades\Socialite;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-auth/callback', function () {
+    $user = Socialite::driver('google')->user();
+ 
+    // $user->token
 });
--- backendrifapp/routes/web.php
+++ backendrifapp/routes/web.php
@@ -1,18 +1,65 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\OauthAccessToken;
+use Illuminate\Support\Carbon;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->stateless()->user();
+
+    $user = User::updateOrCreate([
+        'google_id'=> $user_google->id,
+    ], [
+        'name'=>$user_google->name,
+        'email'=>$user_google->email,
+    ]);
+
+    
+    Auth::login($user);
+    $token = $user->createToken('token')->plainTextToken;
+    $tokenexpire= OauthAccessToken::where('tokenable_id',$user->id)->get()->last();
+    $tokenexpire->expires_at=Carbon :: now ( )->addHour(2);
+    $tokenexpire->update();
+
+    $cookie = cookie('cookie_token',$token,60*1);
+    return response(['token'=>$token, 'usuario'=>$user,'menssage'=>'Login correcto','code'=>'200'])->withoutCookie($cookie);
+//s    $token= $user->createToken('token')->plainTextToken;
+
+    return response()->json(['token' => $token]);
+ 
+    // $user->token
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
