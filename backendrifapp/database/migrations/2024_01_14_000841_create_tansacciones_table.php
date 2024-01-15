275136e3eff5bcb89aa6bdf1e530ba904a71bef5
GuidoAdrian97 <guidoadrian97@hotmail.com>
2024-01-15 17:43:14 -0500

    bug 10

--- backendrifapp/database/migrations/2024_01_14_000841_create_tansacciones_table.php
+++ backendrifapp/database/migrations/2024_01_14_000841_create_tansacciones_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->dateTime('fecha_transaccion');
             $table->foreignId('type_tansacciones_id')->references('id')->on('type_tansacciones');
             $table->timestamps();
-            $table->timestamps();
         });
     }
 
