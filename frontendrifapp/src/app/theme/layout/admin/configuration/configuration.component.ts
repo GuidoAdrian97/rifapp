f6733db86049d772ece54ea2190fe885585fc699
gemalors <monserratloor@yahoo.com>
2024-01-17 00:24:21 -0500

    modificaciones a modal de recarga de saldo

--- frontendrifapp/src/app/theme/layout/admin/configuration/configuration.component.ts
+++ frontendrifapp/src/app/theme/layout/admin/configuration/configuration.component.ts
@@ -12,60 +12,43 @@ export class ConfigurationComponent {
 
  modalRef?: BsModalRef;
   constructor(private modalService: BsModalService) {}
-  mostrarForm:boolean=false;
   mostrarForm1: boolean = false;
   mostrarForm2: boolean = false;
   mostrarForm3: boolean = false;
-  mostrarFormG:boolean=false;
   opciones:boolean=true;
-  titulo:string="";
+  titulo:string="Opciones para recargar saldo.";
 
 
 
   openModalWithClass(template: TemplateRef<void>) {
-    this.opciones=true;
-    this.mostrarFormG=false;
-    this.titulo="Opciones para recargar saldo.";
-    this.mostrarForm=false;
-    this.mostrarForm1=false;
-    this.mostrarForm2=false;
-    this.mostrarForm3=false;
+   
     this.modalRef = this.modalService.show(template, Object.assign({}, { class: 'gray modal-lg' }));
   }
 
-  
+
+  openModalWithClass1(template1: TemplateRef<void>) {
+ 
+    this.modalRef = this.modalService.show(template1, Object.assign({}, { class: 'gray modal-lg' }));
+  }
+
   toggleParrafo1() {
-    this.mostrarFormG=true;
     this.mostrarForm1 = true;
-    this.mostrarForm = true;
-    this.titulo="Recargar saldo por transferencia bancaria.";
-    this.opciones=false;
+    this.mostrarForm2=false;
+    this.mostrarForm3=false;
+    
   }
 
   toggleParrafo2() {
-    this.mostrarFormG=true;
     this.mostrarForm2 = true;
-    this.mostrarForm=true;
-    this.titulo="Recargar saldo por Paypal.";
-    this.opciones=false;
+    this.mostrarForm1=false;
+    this.mostrarForm3=false;
   }
 
   toggleParrafo3() {
-    this.mostrarFormG=true;
     this.mostrarForm3 = true;
-    this.mostrarForm=true;
-    this.titulo="Recargar saldo por tarjetas.";
-    this.opciones=false;
-  }
- 
-  Cancelar() {
-    this.mostrarFormG=false;
-    this.opciones=true;
-    this.mostrarForm =false;
-    this.mostrarForm1 =false;
     this.mostrarForm2=false;
-    this.mostrarForm3=false;
-    this.titulo="Opciones para recargar saldo.";
+    this.mostrarForm1=false;
+  
   }
 
 }
