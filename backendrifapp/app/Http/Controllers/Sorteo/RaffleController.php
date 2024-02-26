3ab4f8abbf17dad20ca2c55685585ecaa3b81a57
GuidoAdrian97 <guidoadrian97@hotmail.com>
2024-02-26 17:07:05 -0500

    premios bug3

--- backendrifapp/app/Http/Controllers/Sorteo/RaffleController.php
+++ backendrifapp/app/Http/Controllers/Sorteo/RaffleController.php
@@ -61,7 +61,7 @@ class RaffleController extends Controller
         $rifa->metodo_sorteo_id = $request->metodo_sorteo_id;
 
         $user->raffle()->save($rifa);
-        dd($request);
+        return response()->json(['mensaje' => 'Rifa creada exitosamente', 'rifa' => $request], 201);
         foreach ($request->premios as $premioData) {
             $premio = new Prize();
             $premio->name_prize = $premioData->name_prize;
