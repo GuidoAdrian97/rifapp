942144701c11608324dbeed2e43af92e7231593c
GuidoAdrian97 <guidoadrian97@hotmail.com>
2024-02-27 01:05:51 -0500

    manejo imagenes

--- backendrifapp/app/Http/Controllers/Sorteo/RaffleController.php
+++ backendrifapp/app/Http/Controllers/Sorteo/RaffleController.php
@@ -76,14 +76,18 @@ class RaffleController extends Controller
             $urlimagenes = [];
     
             foreach ($premioData['imagenes'] as $imagen) {
+                // Decodificar la imagen base64 y guardarla como un archivo
+                $imagenDecodificada = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $imagen));
                 $nombre = time() . '_' . $user->identificacion;
-                $rutaImagen = $rutaUsuario;
+                $rutaImagen = $rutaUsuario . '/' . $nombre;
                 $rutaImagenHEIF = $rutaUsuario . '/' . $nombre . '.heic';
-
-                $imagen->move($rutaUsuario, $nombre);
-    
+            
+                // Guardar la imagen decodificada en el sistema de archivos
+                file_put_contents($rutaImagen, $imagenDecodificada);
+            
+                // Convertir la imagen a formato HEIF si es necesario
                 exec("convert $rutaImagen $rutaImagenHEIF");
-    
+            
                 if (File::exists($rutaImagenHEIF)) {
                     $urlimagenes[] = '/images/' . $cedula . '/' . $nombre . '.heic';
                     File::delete($rutaImagen);
